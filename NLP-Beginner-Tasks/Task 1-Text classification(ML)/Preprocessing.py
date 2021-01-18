6ab634d6b51dccad91c0f81a140aee5884914231
PM <376755744@qq.com>
2021-01-18 13:14:08 +0800

    use TF-IDF to represent Text

--- NLP-Beginner-Tasks/Task 1-Text classification(ML)/Preprocessing.py
+++ NLP-Beginner-Tasks/Task 1-Text classification(ML)/Preprocessing.py
@@ -6,15 +6,18 @@
 
 from LR_Config import LrConfig
 from sklearn import model_selection
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.externals import joblib
 import jieba
 import numpy as np
 
 
 class DataProcess(object):
-    def __init__(self, dataset_path=None, stopwords_path=None):
+    def __init__(self, dataset_path=None, stopwords_path=None, model_save_path=None):
         self.dataset_path = dataset_path
         self.stopwords_path = stopwords_path
+        self.model_save_path = model_save_path
 
     def read_data(self):
         """读取数据"""
@@ -53,6 +56,15 @@ class DataProcess(object):
         label_one_hot = label_one_hot.toarray()
         return model_selection.train_test_split(text_list, label_one_hot, test_size=test_size, random_state=1024)
 
+
+    def get_tfidf(self, X_train, X_test):
+        """使用tfidf实现文本特征提取，将其表示为向量形式"""
+        vectorizer = TfidfVectorizer(min_df=100)
+        vectorizer.fit_transform(X_train)
+        X_train_vec = vectorizer.transform(X_train)
+        X_test_vec = vectorizer.transform(X_test)
+        return X_train_vec, X_test_vec, vectorizer
+
     # TODO:使用word2vec来代替one-hot进行文本表示
     def get_word2vec(self):
         """提取word2vec特征"""
@@ -62,6 +74,8 @@ class DataProcess(object):
         """提供数据"""
         data, stopwords = self.read_data()
         X_train, X_test, y_train, y_test = self.pre_data(data, stopwords, test_size=0.2)
+        X_train_vec, X_test_vec, vectorizer = self.get_tfidf(X_train, X_test)
+        joblib.dump(vectorizer, self.model_save_path)
         return X_train, X_test, y_train, y_test
 
     def batch_iter(self, x, y, batch_size=64):
